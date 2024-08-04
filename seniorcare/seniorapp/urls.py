e8772083023a6b296b9323f8c38696d8fc225030
llenny18 <ramosalleneid01@gmail.com>
2024-08-04 12:53:33 +0800

    fixes

--- seniorcare/seniorapp/urls.py
+++ seniorcare/seniorapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index', views.home, name='index'),
     path('citizens', views.citizens, name='citizens'),
     path('announcements', views.announcements, name='announcements'),
     path('admins', views.admins, name='admins'),
